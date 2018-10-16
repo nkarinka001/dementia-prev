69148cb616127713b1cc5bed7507fc2b76a7f812
nkarinka001 <nkarinka9@gmail.com>
2018-10-16 15:00:40 +0800

    route from home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
 import Home from './components/Game/Home';
-import StartGame from './components/Game/StartGame';
+import Guide from './components/Game/Guide';
 import { Switch, Route , Redirect} from 'react-router-dom';
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <Switch>
         <Route path='/home' component={Home} />
-        <Route path='/start-game' component={StartGame} />
+        <Route path='/start-game' component={Guide} />
         <Route render={() => <Redirect to='/home' />} />
       </Switch>
     );
